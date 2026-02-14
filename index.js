ef53fcd590b49a0ff6034d68f35bb242ee5f5b54
atlaskey <46149020+atlaskey@users.noreply.github.com>
2026-02-15 00:00:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const { WebSocketServer } = require('ws');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'www')));
 
